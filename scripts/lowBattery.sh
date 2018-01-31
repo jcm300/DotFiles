78be7c11e34c8800de8d42ad969ae48049125d95
jcm300 <zeclmartins@gmail.com>
2018-01-31 23:11:41 +0000

    script fixed

--- scripts/lowBattery.sh
+++ scripts/lowBattery.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-BATTINFO=acpi -b
-if [[ echo $BATTINFO | grep Discharging && echo $BATTINFO | cut -f 5 -d " " < 00:15:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
+BATTINFO=$(acpi -b)
+BATTGREP=$(echo "$BATTINFO" | grep Discharging)
+BATTLEVEL=$(echo "$BATTINFO" | grep -P -o '[0-9]+(?=%)')
+if [[ "$BATTLEVEL" -le 15 && "$BATTGREP" == "$BATTINFO" ]];
+then
+    /usr/bin/notify-send "Low Battery" "$BATTINFO"
 fi
