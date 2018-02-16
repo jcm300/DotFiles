74cd9ed8bb7cf19e38c44831c259b812cd6f1234
jcm300 <zeclmartins@gmail.com>
2018-02-16 16:41:06 +0000

    fixed battery warning script

--- scripts/lowBattery.sh
+++ scripts/lowBattery.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 BATTINFO=$(acpi -b)
 BATTGREP=$(echo "$BATTINFO" | grep Discharging)
 BATTLEVEL=$(echo "$BATTINFO" | grep -P -o '[0-9]+(?=%)')
 if [[ "$BATTLEVEL" -le 15 && "$BATTGREP" == "$BATTINFO" ]];
 then
+    export DISPLAY=:0
     /usr/bin/notify-send "Low Battery" "$BATTINFO"
 fi
